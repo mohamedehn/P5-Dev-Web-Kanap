0009e3c5acbd3283e7831acf9f6e9e73fb2bcd8e
Mohamed <mohamed.ebarhmatin@yahoo.fr>
2022-08-30 20:34:41 +0100

    Step 6 checked

--- front/product.js
+++ front/product.js
@@ -2,6 +2,7 @@
 let params = new URLSearchParams (document.location.search)
 let id = params.get("id")
 
+// Récupération de l'ID d'un produit ainsi que le détails de chaque produit
 getDetailProduct(id)
 
 function getDetailProduct(productId){
@@ -14,13 +15,24 @@ function getDetailProduct(productId){
       }
 
 function createDetailProduct(product){
-  let imgProduct = document.getElementsByClassName("item__img");
-  const afficherImgProduct = product.imageUrl;
-  imgProduct.src = afficherImgProduct;
+  const img = document.createElement("img");
+  const afficherImg = product.imageUrl;
+  img.src = afficherImg;
+  let itemImg = document.querySelector(".item__img");
+  itemImg.appendChild(img);
   let titleProduct = document.getElementById("title");
   titleProduct.textContent = product.name;
   let descriptionProduct = document.getElementById("description");
   descriptionProduct.textContent = product.description;
+  let price = document.getElementById("price")
+  price.textContent = product.price
+  let couleur = document.getElementById("colors");
+  for(choice = 0 ; choice < product.colors.length; choice++){
+    let choiceColors = document.createElement("option");
+    choiceColors.textContent = product.colors[choice]
+    couleur.appendChild(choiceColors);
+  }
+  
 }
 
 let panier = localStorage.setItem('panier',JSON.stringify([2,3,4,5]))
